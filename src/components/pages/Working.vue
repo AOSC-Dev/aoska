b2fe1d21142ae1e4ba020ffae9f7377e6150d592
yangzongkainj <yangzongkainj@163.com>
2025-07-13 22:23:13 +0800

    change: optimize the design of category pages

--- src/components/pages/Working.vue
+++ src/components/pages/Working.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="working">
+    <div class="ranking">
+      <span>{{ $t("pages.ranking") }}</span>
+      <span>{{ $t("pages.name") }}</span>
+      <span>{{ $t("pages.download") }}</span>
+      <span>{{ $t("pages.update") }}</span>
+    </div>
     <AppBanner
       v-for="(app, index) in appList"
       :key="index"
@@ -28,32 +34,32 @@ interface AppInfo {
 }
 
 // 获取应用列表
-// const appList = [
-//   {
-//     name: "WPS 办公套件",
-//     intro: "这是应用程序 WPS，这是应用程序 WPS 办公套件",
-//     version: "123.4.5",
-//     size: "233 Mib"
-//   },
-//   {
-//     name: "WPS 办公套件",
-//     intro: "这是应用程序 WPS，这是应用程序 WPS 办公套件",
-//     version: "123.4.5",
-//     size: "233 Mib"
-//   }
-// ]
-
-const appList = ref<AppInfo[]>([])
-const fetchAppList = async (category: string) => {
-  try {
-    const result = await invoke<AppInfo[]>('fetch_by_category', { 
-      category: category 
-    });
-    appList.value = result;
-  } catch (error) {
-    console.error('Error fetching list:', error);
+const appList = [
+  {
+    name: "WPS 办公套件",
+    intro: "这是应用程序 WPS，这是应用程序 WPS 办公套件",
+    version: "123.4.5",
+    size: "233 Mib"
+  },
+  {
+    name: "WPS 办公套件",
+    intro: "这是应用程序 WPS，这是应用程序 WPS 办公套件",
+    version: "123.4.5",
+    size: "233 Mib"
   }
-}
+]
+
+// const appList = ref<AppInfo[]>([])
+// const fetchAppList = async (category: string) => {
+//   try {
+//     const result = await invoke<AppInfo[]>('fetch_by_category', { 
+//       category: category 
+//     });
+//     appList.value = result;
+//   } catch (error) {
+//     console.error('Error fetching list:', error);
+//   }
+// }
 
 // 跳转到应用详情
 const showDetail = () => {
@@ -61,14 +67,26 @@ const showDetail = () => {
 }
 
 // 组件挂载时自动执行
-const category = "working"
-onBeforeMount(() => {
-  fetchAppList(category)
-})
+// const category = "working"
+// onBeforeMount(() => {
+//   fetchAppList(category)
+// })
 </script>
 
 <style scoped>
 .working {
   margin: 60px 20px 20px 20px;
+}
+
+.ranking {
+  height: 40px;
+  font-size: 18px;
+  line-height: 40px;
+  background-color: rgb(206, 233, 255);
+  border-radius: 5px 5px 0 0;
+}
+
+.ranking span {
+  margin-left: 10px;
 }
 </style>
