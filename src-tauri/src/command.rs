435b3eefdccff6f652c854c5db8b6c7aaaa3f81b
wxiwnd <wxiwnd@outlook.com>
2025-07-15 23:59:06 +0800

    add: introduce oma-pm
    Signed-off-by: wxiwnd <wxiwnd@outlook.com>
    Signed-off-by: wxiwnd <wxiwnd@aosc.io>

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,11 +1,16 @@
 use crate::common::{
+    config::{ASM_INDEX_PATH, ASM_RECOMMEND_INDEX_PATH},
     index::{CategoryIndex, Index, RecommendIndex},
+    oma::{
+        check_security_upgradable, check_security_upgradable_detail, check_upgradable,
+        check_upgradable_detail,
+    },
     packages::{Category, PackageDetail},
     utils::fetch_data,
 };
 
 use anyhow::Result;
-use tauri::App;
+use oma_pm::apt::{AptConfig, OmaApt, OmaAptArgs};
 
 pub struct AppState {
     client: reqwest::Client,
@@ -24,7 +29,7 @@ impl AppState {
 
 #[tauri::command]
 pub async fn fetch_recommend(app: tauri::State<'_, AppState>) -> Result<RecommendIndex, String> {
-    fetch_data(&app.client, "recommend_index.json")
+    fetch_data(&app.client, ASM_RECOMMEND_INDEX_PATH)
         .await
         .map_err(|e| e.to_string())
 }
@@ -34,7 +39,7 @@ pub async fn fetch_by_category(
     app: tauri::State<'_, AppState>,
     category: String,
 ) -> Result<CategoryIndex, String> {
-    let index_data: Index = fetch_data(&app.client, "aoska_index.json")
+    let index_data: Index = fetch_data(&app.client, ASM_INDEX_PATH)
         .await
         .map_err(|e| e.to_string())?;
     let cat = category
@@ -50,7 +55,7 @@ pub async fn fetch_by_category(
 
 #[tauri::command]
 pub async fn fetch_index(app: tauri::State<'_, AppState>) -> Result<Index, String> {
-    fetch_data(&app.client, "aoska_index.json")
+    fetch_data(&app.client, ASM_INDEX_PATH)
         .await
         .map_err(|e| e.to_string())
 }
@@ -60,8 +65,36 @@ pub async fn fetch_detail(
     app: tauri::State<'_, AppState>,
     pkg_name: String,
 ) -> Result<PackageDetail, String> {
-    let path = format!("pkgs/{pkg_name}");
+    let path = format!("packages/{pkg_name}/meta.json");
     fetch_data(&app.client, &path)
         .await
         .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub async fn fetch_update() -> Result<usize, String> {
+    let apt = OmaApt::new(
+        vec![],
+        OmaAptArgs::builder().build(),
+        false,
+        AptConfig::new(),
+    )
+    .map_err(|e| e.to_string())?;
+    let count = check_upgradable(&apt).await.map_err(|e| e.to_string())?;
+    Ok(count)
+}
+
+#[tauri::command]
+pub async fn fetch_security_update() -> Result<usize, String> {
+    let apt = OmaApt::new(
+        vec![],
+        OmaAptArgs::builder().build(),
+        false,
+        AptConfig::new(),
+    )
+    .map_err(|e| e.to_string())?;
+    let count = check_security_upgradable(&apt)
+        .await
+        .map_err(|e| e.to_string())?;
+    Ok(count)
 }
