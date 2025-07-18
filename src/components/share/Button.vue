f17f312d03190c0ede020065c3994a1dbc8d1e65
SparrowHe <sparrowhe@gmail.com>
2025-07-18 23:32:35 +0800

    fix: change Button component styles

--- src/components/share/Button.vue
+++ src/components/share/Button.vue
@@ -1,8 +1,38 @@
 <template>
-  <button class="button">
+  <button 
+    :class="['button', size, color]" 
+    :style="{ 
+      width: width ? (isNaN(width) ? width : width + 'px') : null,
+      height: height ? (isNaN(height) ? height : height + 'px') : null,
+      lineHeight: height ? (isNaN(height) ? height : height + 'px') : null
+    }"
+  >
     <slot></slot>
   </button>
 </template>
+
+<script setup>
+defineProps({
+  size: {
+    type: String,
+    default: '',
+    validator: (value) => ['small', 'medium', 'large', ''].includes(value)
+  },
+  color: {
+    type: String,
+    default: '',
+    validator: (value) => ['green', 'grey', 'yellow', 'red', ''].includes(value)
+  },
+  width: {
+    type: [String, Number],
+    default: null
+  },
+  height: {
+    type: [String, Number],
+    default: null
+  }
+});
+</script>
 
 <style scoped>
 .button {
@@ -11,5 +41,31 @@
   border-radius: 5px;
   font-size: 18px;
   text-align: center;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.small {
+  font-size: 14px;
+}
+
+.medium {
+  font-size: 18px;
+}
+
+.green {
+  background-color: #ceffd6;
+}
+
+.grey {
+  background-color: #f3f3f3;
+}
+
+.yellow {
+  background-color: #fad8b9;
+}
+
+.red {
+  background-color: #ffd9ce;
 }
 </style>
